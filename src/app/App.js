0ae0fd05f6b32265d4dbbb429c67ee75f7cfff8f
cmmerritt <christiane.merritt@gmail.com>
2021-04-30 13:44:35 -0700

    working search function

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import PokeList from '../PokeList';
 import request from 'superagent';
+import Search from '../Search';
 import './App.css';
+
+const url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
 class App extends Component {
   state = {
-    pokemons: [],
+    pokemon: [],
   }
 
-  async componentDidMount() {
-    const url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+  //moved async after componentDidMount and made into arrow function why???
+
+  componentDidMount = async () => {
     const response = await request.get(url);
-    this.setState({ pokemons: response.body.results });
+    this.setState({ pokemon: response.body.results });
+  }
+
+  handleSearch = async ({ search }) => {
+    const response = await request
+      .get(url)
+      .query({ pokemon: search });
+
+    this.setState({ pokemon: response.body.results });
   }
   
   render() {
+    const { pokemon } = this.state;
     return (
       <div className="App">
 
+        <section className="search-options">  
+          <Search onSearch={this.handleSearch}/>
+          {/* <Paging/> */}
+        </section>
+
         <main>
-          <PokeList pokes={this.state.pokemons}/>
+          <PokeList pokemon={this.state.pokemon}/>
         </main>
       
       </div>
